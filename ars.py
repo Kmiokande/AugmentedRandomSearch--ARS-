f71392867a9d1af94cde062c5e94feba6ba413de
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-08 01:30:47 -0300

    Function to calculate mean and variance

--- ars.py
+++ ars.py
@@ -22,3 +22,10 @@ class Normalizer():
         self.mean = np.zeros(nb_inputs)
         self.mean_diff = np.zeros(nb_inputs)
         self.var = np.zeros(nb_inputs)
+
+    def observe(self, x):
+        self.n += 1.0
+        last_mean = self.mean.copy()
+        self.mean += (x - self.mean) / self.n
+        self.mean_diff += (x - last_mean) * (x - self.mean)
+        self.var = (self.mean_diff / self.n).clip(min=1e-2) # 0.001
