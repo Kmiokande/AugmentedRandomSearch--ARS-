16a9ec9ed5066fa9f346f631eff0479f898cfcf0
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-08 01:14:48 -0300

    Change Camel Case to Snake Case

--- ars.py
+++ ars.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class Hp():
 	def __init__(self):
-		self.nbSteps = 1000
-		self.episodeLenght = 100
-		self.learningRate = 0.02
-		self.nbDirections = 16
-		self.nbBestDirections = 16
-		assert self.nbBestDirections <= self.nbDirections
+		self.nb_steps = 1000
+		self.episode_lenght = 100
+		self.learning_rate = 0.02
+		self.nb_directions = 16
+		self.nb_best_directions = 16
+		assert self.nb_best_directions <= self.nb_directions
 		self.noise = 0.03
 		self.seed = 1
-		self.envName = ''
+		self.env_name = ''
