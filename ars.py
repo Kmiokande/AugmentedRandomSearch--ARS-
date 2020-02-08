3290f7382fd13619acc96516d864ba7b00f435f5
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-08 16:26:44 -0300

    Create functions evaluate, sampleDeltas and update

--- ars.py
+++ ars.py
@@ -41,3 +41,23 @@ class Normalizer():
 class Policy():
     def __init__(self, input_size, output_size):
         self.theta = np.zeros((output_size, input_size))
+
+    def evaluate(self, input, delta=None, direction=None):
+        if direction is None:
+            return self.theta.dot(input)
+        elif direction == 'positive':
+            return (self.theta + hp.noise * delta).dot(input)
+        else:
+            return (self.theta - hp.noise * delta).dot(input)
+
+    def sampleDeltas(self):
+        return [np.random.randn(*self.theta.shape) for _ in range(hp.nb_directions)]
+
+    # rollout [recompensa posita, recompensa negativa, perturbação]
+    # sigma_r -> desvio padrão da recompensa
+    def update(self, rollouts, sigma_r):
+        step = np.zeros(self.theta.shape)
+        for r_pos, r_neg, d in rollouts:
+            step += (r_pos + r_neg) * d
+        self.theta += hp.learning_rate / \
+            (hp.nb_best_directions * sigma_r) * step
