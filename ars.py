fee6fc0d83d5b04e0ab9010ed33da53cd3675767
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-08 01:22:04 -0300

    Create Normalizer class

--- ars.py
+++ ars.py
@@ -3,13 +3,22 @@ import numpy as np
 
 
 class Hp():
-	def __init__(self):
-		self.nb_steps = 1000
-		self.episode_lenght = 100
-		self.learning_rate = 0.02
-		self.nb_directions = 16
-		self.nb_best_directions = 16
-		assert self.nb_best_directions <= self.nb_directions
-		self.noise = 0.03
-		self.seed = 1
-		self.env_name = ''
+    def __init__(self):
+        self.nb_steps = 1000
+        self.episode_lenght = 100
+        self.learning_rate = 0.02
+        self.nb_directions = 16
+        self.nb_best_directions = 16
+        assert self.nb_best_directions <= self.nb_directions
+        self.noise = 0.03
+        self.seed = 1
+        self.env_name = ''
+
+
+# Normalização dos estados
+class Normalizer():
+    def __init__(self, nb_inputs):
+        self.n = np.zeros(nb_inputs)
+        self.mean = np.zeros(nb_inputs)
+        self.mean_diff = np.zeros(nb_inputs)
+        self.var = np.zeros(nb_inputs)
