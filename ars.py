cc0c650d3c8fde1b868d563a4a0b341720fa90fc
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-02-10 01:53:28 -0300

    Environmental exploration function

--- ars.py
+++ ars.py
@@ -61,3 +61,19 @@ class Policy():
             step += (r_pos + r_neg) * d
         self.theta += hp.learning_rate / \
             (hp.nb_best_directions * sigma_r) * step
+
+
+def explore(env, normalizer, policy, direction=None, delta=None):
+    state = env.reset()
+    done = False
+    num_plays = 0.0
+    sum_rewards = 0
+    while not done and num_plays < hp.episode_lenght:
+        normalizer.observe(state)
+        state = normalizer.normalize(state)
+        action = policy.evaluate(state, delta, direction)
+        state, reward, done, _ = env.step(action)
+        reward = max(min(reward, 1), -1)
+        sum_rewards += reward
+        num_plays += 1
+    return sum_rewards
